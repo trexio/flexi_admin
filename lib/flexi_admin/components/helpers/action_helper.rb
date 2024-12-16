8d2c81c1a1c19bf657b73bf22b4af3216abab931
Tomas Landovsky <landovsky@gmail.com>
2024-12-16 10:11:25 +0100

    Use action button in standalone fashion

--- lib/flexi_admin/components/helpers/action_helper.rb
+++ lib/flexi_admin/components/helpers/action_helper.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 module FlexiAdmin::Components::Helpers::ActionHelper
-  def action(action_component, disabled: true, selection_dependent: true)
-    render FlexiAdmin::Components::Resources::BulkAction::ButtonComponent.new(context,
+  # Context overrides the context normally given by the class including this module
+  # Example - adding action button to a custom view form
+  def action(action_component, disabled: true, selection_dependent: true, context: nil)
+    render FlexiAdmin::Components::Resources::BulkAction::ButtonComponent.new(context || self.context,
                                                                               action_component,
                                                                               disabled:,
                                                                               selection_dependent:)
